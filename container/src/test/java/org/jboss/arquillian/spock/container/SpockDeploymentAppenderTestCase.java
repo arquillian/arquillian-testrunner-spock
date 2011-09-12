d986784595b5e745ddaaae723a24c77108bb5cb7
Bartosz Majsak <bartosz.majsak@gmail.com>
2011-09-12 22:16:37 +0200

    First version of Arquillian and Spock integration.

---
+++ container/src/test/java/org/jboss/arquillian/spock/container/SpockDeploymentAppenderTestCase.java
@@ -0,0 +1,104 @@
+/*
+ * JBoss, Home of Professional Open Source
+ * Copyright 2009, Red Hat Middleware LLC, and individual contributors
+ * by the @authors tag. See the copyright.txt in the distribution for a
+ * full listing of individual contributors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jboss.arquillian.spock.container;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Scanner;
+
+import org.jboss.shrinkwrap.api.Archive;
+import org.jboss.shrinkwrap.api.ArchivePath;
+import org.jboss.shrinkwrap.api.ArchivePaths;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ *
+ * @author <a href="mailto:bartosz.majsak@gmail.com">Bartosz Majsak</a>
+ * 
+ * @version $Revision: $
+ */
+public class SpockDeploymentAppenderTestCase
+{
+
+   @Test
+   public void shouldAddSpockExtensionAsServiceProvider() throws Exception 
+   {
+      // given
+      SpockDeploymentAppender spockDeploymentAppender = new SpockDeploymentAppender();
+      ArchivePath spockExtensionSPI = ArchivePaths.create("/META-INF/services/org.spockframework.runtime.extension.IGlobalExtension");
+      
+      // when
+      Archive<?> archive = spockDeploymentAppender.createAuxiliaryArchive();
+      
+      // then
+      Assert.assertTrue("Should have added Spock Extension",
+            archive.contains(spockExtensionSPI));
+      
+      Assert.assertEquals("Should register Arquillian extension as Spock Extension", 
+            "org.jboss.arquillian.spock.ArquillianSpockExtension", 
+            getResourceContent(archive, spockExtensionSPI));
+   }
+
+   @Test
+   public void shouldAddSpockTestRunnerAsServiceProvider() throws Exception 
+   {
+      // given
+      SpockDeploymentAppender spockDeploymentAppender = new SpockDeploymentAppender();
+      ArchivePath testRunnerSPI = ArchivePaths.create("/META-INF/services/org.jboss.arquillian.container.test.spi.TestRunner");
+      
+      // when
+      Archive<?> archive = spockDeploymentAppender.createAuxiliaryArchive();
+      
+      // then
+      Assert.assertTrue("Should have added Test Runner",
+            archive.contains(testRunnerSPI));
+      
+      Assert.assertEquals("Should have registered Spock Test Runner as Arquillian Test Runner", 
+            "org.jboss.arquillian.spock.container.SpockTestRunner", 
+            getResourceContent(archive, testRunnerSPI));
+   }
+
+   /**
+   .addAsServiceProvider(ScriptEngineFactory.class, GroovyScriptEngineFactory.class)
+* @throws Exception
+*/
+   
+   // Private utility classes
+   
+   private String getResourceContent(Archive<?> archive, ArchivePath path)
+   {
+      final InputStream openStream = archive.get(path).getAsset().openStream();
+      String content = "";
+      try
+      {
+         content = new Scanner(openStream).useDelimiter("\\A").next();
+         return content.trim();
+      }
+      finally
+      {
+         try
+         {
+            openStream.close();
+         }
+         catch (IOException ignore)
+         {
+            // NOOP
+         }
+      }
+   }
+}
